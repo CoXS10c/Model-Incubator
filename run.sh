e467b1075b3bdd63d7c9b983b32a5804b60f5502
david <davidtnfsh@gmail.com>
2018-12-09 23:02:56 +0800

    <chores>: update args name

--- run.sh
+++ run.sh
@@ -7,8 +7,12 @@ case $a in
     input="${a#*=}"
     shift # past argument=value
     ;;
-    -o=*|--output=*)
-    output="${a#*=}"
+    -eo=*|--extout=*)
+    extract_output="${a#*=}"
+    shift # past argument=value
+    ;;
+    -co=*|--convout=*)
+    convert_output="${a#*=}"
     shift # past argument=value
     ;;
     -s=*|--src=*)
@@ -27,6 +31,10 @@ case $a in
     folder="${a#*=}"
     shift # past argument=value
     ;;
+    -tra=*|--tradition=*)
+    tradition="${a#*=}"
+    shift # past argument=value
+    ;;
     --default)
     DEFAULT=YES
     shift # past argument with no value
@@ -37,14 +45,18 @@ case $a in
 esac
 done
 echo "input=${input}"
-echo "output=${output}"
+echo "extract_output=${extract_output}"
 echo "src=${src}"
 echo "dst=${dst}"
 echo "model=${model}"
 echo "folder=${folder}"
+echo "tradition=${tradition}"
 
 
-python3 faceswap.py extract -i ${input} -o ${output} -m ${model}
-python3 head_post_estimation.py -f ${folder}
-python3 faceswap.py convert -i ${input} -o ${output} -m ${model}
-python main.py --src ${from} --dst ${dst} --out ${output} --correct_color
+python3 faceswap.py extract -i ${input} -o ${extract_output} -m ${model}
+python3 faceswap.py convert -i ${input} -o ${convert_output} -m ${model}
+if [ $tradition -eq 1 ]
+then
+    python3 head_post_estimation.py -f ${dst}
+    python main.py --src ${from} --dst ${dst} --out ${convert_output} --correct_color
+fi
