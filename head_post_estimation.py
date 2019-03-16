fe40aaf63670abb6f58cc3f8a3069f6c67439b19
david <davidtnfsh@gmail.com>
2019-03-16 10:51:08 +0800

    <refactor/style>: use yapf + remove cv.read this type check cause i've ignored hidden file

--- head_post_estimation.py
+++ head_post_estimation.py
@@ -10,20 +10,37 @@ import argparse
 import tqdm
 from scipy.spatial.distance import cosine as cosineDS
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-PARSER = argparse.ArgumentParser(description='Search picture with the most similar angle')
-PARSER.add_argument('-f', '--folder', required=True, dest='folder',
-                    help='text input to search for')
-PARSER.add_argument('-i', '--img', required=False, dest='img',
-                    help='the image which would be used to calculate similarity between other images')
+PARSER = argparse.ArgumentParser(
+    description='Search picture with the most similar angle')
+PARSER.add_argument(
+    '-f',
+    '--folder',
+    required=True,
+    dest='folder',
+    help='text input to search for')
+PARSER.add_argument(
+    '-i',
+    '--img',
+    required=False,
+    dest='img',
+    help=
+    'the image which would be used to calculate similarity between other images'
+)
 ARGS = PARSER.parse_args()
 
-sess = tf.Session() #Launch the graph in a session.
-my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+sess = tf.Session()  #Launch the graph in a session.
+my_head_pose_estimator = CnnHeadPoseEstimator(
+    sess)  #Head pose estimation object
 
 # Load the weights from the configuration folders
-my_head_pose_estimator.load_roll_variables(os.path.realpath("deepgaze/etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
-my_head_pose_estimator.load_pitch_variables(os.path.realpath("deepgaze/etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
-my_head_pose_estimator.load_yaw_variables(os.path.realpath("deepgaze/etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"))
+my_head_pose_estimator.load_roll_variables(
+    os.path.realpath(
+        "deepgaze/etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
+my_head_pose_estimator.load_pitch_variables(
+    os.path.realpath(
+        "deepgaze/etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
+my_head_pose_estimator.load_yaw_variables(
+    os.path.realpath("deepgaze/etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"))
 
 angle_dict = {}
 if ARGS.img:
@@ -36,8 +53,11 @@ if ARGS.img:
         image)  # Evaluate the pitch angle using a CNN
     yaw = my_head_pose_estimator.return_yaw(
         image)  # Evaluate the yaw angle using a CNN
-    image_angel = [float(
-        roll[0, 0, 0]), float(pitch[0, 0, 0]), float(yaw[0, 0, 0])]
+    image_angel = [
+        float(roll[0, 0, 0]),
+        float(pitch[0, 0, 0]),
+        float(yaw[0, 0, 0])
+    ]
 
 for file_name in tqdm.tqdm(Path(ARGS.folder).glob('**/*')):
     suffix = file_name.suffix.lower()
@@ -45,13 +65,20 @@ for file_name in tqdm.tqdm(Path(ARGS.folder).glob('**/*')):
         continue
     image = cv2.imread(str(file_name))  # Read the image with OpenCV
     # Get the angles for roll, pitch and yaw
-    roll = my_head_pose_estimator.return_roll(image)  # Evaluate the roll angle using a CNN
-    pitch = my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
-    yaw = my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
-    angle_dict[str(file_name)] = [float(roll[0,0,0]), float(pitch[0,0,0]), float(yaw[0,0,0])]
+    roll = my_head_pose_estimator.return_roll(
+        image)  # Evaluate the roll angle using a CNN
+    pitch = my_head_pose_estimator.return_pitch(
+        image)  # Evaluate the pitch angle using a CNN
+    yaw = my_head_pose_estimator.return_yaw(
+        image)  # Evaluate the yaw angle using a CNN
+    angle_dict[str(file_name)] = [
+        float(roll[0, 0, 0]),
+        float(pitch[0, 0, 0]),
+        float(yaw[0, 0, 0])
+    ]
     if ARGS.img:
-        most_similar[str(file_name)] = cosineDS(
-            angle_dict[str(file_name)], image_angel)
+        most_similar[str(file_name)] = cosineDS(angle_dict[str(file_name)],
+                                                image_angel)
 
 json.dump(angle_dict, open(str(ARGS.folder) + '.json', 'w'))
 
